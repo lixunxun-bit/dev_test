8ad7adf4c6def5c8d15bebcc52173b35fbcd403a
leexun <leslie.leexun@qq.com>
2025-11-29 21:09:02 +0800

    补全了函数

--- code/test.c
+++ code/test.c
@@ -18,6 +18,17 @@ void swap(int *a, int *b)
     printf("hello");
 }
 
+int arr_sum(int* arr, int len)
+{
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
 int main()
 {   
     int a = 1;
