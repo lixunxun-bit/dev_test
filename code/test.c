29bc9b3aedf7bfdadf71943b6971a88c5aeb2e3c
leexun <leslie.leexun@qq.com>
2025-11-29 19:52:41 +0800

    修改了test.c文件，增加了swap函数

--- code/test.c
+++ code/test.c
@@ -2,9 +2,21 @@
 
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 
 int main()
-{
+{   
+    int a = 1;
+    int b = 2;
+
+    swap(&a, &b);
+    printf("%d, %d\n", a, b);
 
     printf("Hello, world!\n");
     return 0;
